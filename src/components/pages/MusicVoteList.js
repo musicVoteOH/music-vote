1bda6fdba5eaba7fcbeac5e6a20fe2a633bbdb50
golubs <32750555+golubs@users.noreply.github.com>
2019-11-29 16:25:48 +0100

    Add musiclist (#6)

--- src/components/pages/MusicVoteList.js
+++ src/components/pages/MusicVoteList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../Card";
 import styled from "@emotion/styled";
+import getMusic from "../../api/getMusic";
 
 const MusicVoteListContainer = styled.div`
   display: flex;
@@ -8,10 +9,30 @@ const MusicVoteListContainer = styled.div`
 `;
 
 function MusicVoteList() {
+  const [music, setMusic] = React.useState([]);
+
+  async function refreshMusicList() {
+    const foundMusic = await getMusic();
+    console.log("foundMusic:", foundMusic);
+    setMusic(foundMusic);
+    // return music;
+  }
+
+  React.useEffect(() => {
+    refreshMusicList();
+  }, []);
+
   return (
     <MusicVoteListContainer>
-      <Card />
-      <Card />
+      {music.map(song => (
+        <Card
+          key={song.id}
+          song={song.song}
+          artist={song.artist}
+          comment={song.comment}
+          vote={song.vote}
+        />
+      ))}
     </MusicVoteListContainer>
   );
 }
