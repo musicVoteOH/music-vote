0d30a267f03b53478dfc9fe7527bbfe644fda7fd
holgermanzke <manzkeholger@gmail.com>
2019-11-29 17:45:48 +0100

    Add api to json server

--- src/components/pages/AddMusicVote.js
+++ src/components/pages/AddMusicVote.js
@@ -19,24 +19,66 @@ const Input = styled.input`
 `;
 
 function AddMusicVote() {
+  const [song, setSong] = React.useState("");
+  const [artist, setArtist] = React.useState("");
+  const [comment, setComment] = React.useState("");
+  const [vote, setVote] = React.useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log(song, artist, comment, vote);
+    await fetch("http://localhost:8878/music", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ song, artist, comment, vote })
+    });
+    setSong("");
+    setArtist("");
+    setComment("");
+    setVote("");
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Label>
         Song
-        <Input type="text" autoFocus />
+        <Input
+          type="text"
+          autoFocus
+          value={song}
+          onChange={event => setSong(event.target.value)}
+          required
+        />
       </Label>
       <Label>
         Artist
-        <Input type="text" />
+        <Input
+          type="text"
+          value={artist}
+          onChange={event => setArtist(event.target.value)}
+          required
+        />
       </Label>
       <Label>
         Comment
-        <Input type="textarea" row="30" col="30" />
+        <Input
+          type="textarea"
+          rows="30"
+          value={comment}
+          onChange={event => setComment(event.target.value)}
+        />
       </Label>
       <Label>
         Vote
-        <Input type="number" />
+        <Input
+          type="number"
+          value={vote}
+          onChange={event => setVote(event.target.value)}
+        />
       </Label>
+      <button>Submit</button>
     </Form>
     // <div>AddMusicVote</div>
   );
