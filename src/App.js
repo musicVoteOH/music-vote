11f5b4c6b9844881ab9b3885059c92843a572a4e
golubs <oleggolubs@gmail.com>
2019-11-29 13:17:12 +0100

    Add layout of header with navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { ThemeProvider } from "emotion-theming";
 import GlobalStyles from "./GlobalStyles";
+import styled from "@emotion/styled";
 import theme from "./themes/theme";
-import NavButton from "./components/NavButton";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddMusicVote from "./components/pages/AddMusicVote";
 import MusicVoteList from "./components/pages/MusicVoteList";
+import Header from "./components/Header";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme.default}>
       <GlobalStyles />
       <Router>
-        <NavButton />
-        <Switch>
-          <Route exact path="/">
-            <MusicVoteList />
-          </Route>
-          <Route path="/vote">
-            <AddMusicVote />
-          </Route>
-        </Switch>
+        <Container>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <MusicVoteList />
+            </Route>
+            <Route path="/vote">
+              <AddMusicVote />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
     </ThemeProvider>
   );
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,6 +2,7 @@ const theme = {
   default: {
     background: "linear-gradient(120deg, #FDFBFB 0%, #EBEDEE 100%)",
     text: "#222b22",
+    textLight: "#ffff",
     primary: "#1a48ff",
     secondary: "#baf1fc",
     tertiary: "#262e5b"
