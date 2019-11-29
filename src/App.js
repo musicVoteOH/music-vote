23f91f52df3349c6ffb4bc43da102b121b67bdfb
golubs <32750555+golubs@users.noreply.github.com>
2019-11-29 10:54:55 +0100

    Add GlobalStyles (#2)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import GlobalStyles from "./GlobalStyles";
+import theme from "./themes/theme";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme.default}>
+      <GlobalStyles />
+      <div>Hello</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/themes/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  default: {
+    background: "linear-gradient(120deg, #FDFBFB 0%, #EBEDEE 100%)",
+    text: "#222b22",
+    primary: "#1a48ff",
+    secondary: "#baf1fc",
+    tertiary: "#262e5b"
+  }
+};
+
+export default theme;
